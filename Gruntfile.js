b8aecb7f7ead4b6ec1f4bd4f647ea77def409805
ok2ju <soulSpringg@gmail.com>
2014-12-04 20:13:25 +0200

    add Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            sass: {
+                files: ['**/*.{scss,sass}'],
+                tasks: ['sass']
+            },
+        },
+        sass: {
+            dist: {
+                files: {
+                    'public/styles/main.css': 'public/styles/main.scss',
+                    'public/styles/login.css': 'public/styles/login.scss'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['sass','watch']);
+};
