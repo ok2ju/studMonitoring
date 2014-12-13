3f787c02f504e87b82938554b44f88651ec83b67
alehatsman <olegruno@gmail.com>
2014-12-13 18:36:35 +0300

    revert router

--- routes/students.js
+++ routes/students.js
@@ -2,12 +2,15 @@
  * Created by alehatsman on 12/3/14.
  */
 var express = require('express'),
-    router = express.Router(),
-    student = require('../models/student.js');
+  router = express.Router(),
+  student = require('../models/student.js');
 
 router.get('/', function(request, responce) {
   student.list(function(err, result) {
-    responceWrapper(err, result.rows, responce);
+    if(err) {
+      return responce.send(err);
+    }
+    responce.json(result.rows);
   });
 });
 
@@ -15,29 +18,31 @@ router.get('/:id', function(request, responce) {
   var id = request.params.id;
   console.log("StudentRouter - get id = " + id);
   student.find(id, function(err, result) {
-    responceWrapper(err, result.rows[0], responce);
+    if(err) {
+      return responce.send(err);
+    }
+    responce.json(result.rows[0]);
   });
 });
 
 router.get('/:klass', function(request, responce) {
   var klass = request.params.klass;
   student.classlist(klass, function(err, result) {
-    responceWrapper(err, result.rows, responce);
+    if(err) {
+      return responce.send(err);
+    }
+    responce.json(result.rows);
   });
 });
 
 router.post('/', function(request, responce) {
   var s = request.body;
   student.save(s, function(err, result) {
-    responceWrapper(err, result, responce);
+    if(err) {
+      return responce.send(err);
+    }
+    responce.json(result);
   });
 });
-
-function responceWrapper(err, result, responce) {
-  if(err) {
-    return responce.send(err);
-  }
-  responce.json(result);
-}
 
 module.exports = router;
